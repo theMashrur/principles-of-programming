b977082748964de1f14cf245e47cc21a5e109fe3
Mashrur Khondokar <shafi0802@gmail.com>
2022-04-17 15:46:17 +0100

    added 4th week code

---
+++ week4/life/__init__.py
@@ -0,0 +1,1 @@
+from .life import glider, blinker, glider_gun, Game, Pattern # noqa F401
---
+++ week4/life/life.py
@@ -0,0 +1,115 @@
+import numpy as np # noqa F401
+from matplotlib import pyplot
+from scipy.signal import convolve2d
+
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
+
+blinker = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+
+glider_gun = np.array([
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0]
+])
+
+
+class Game:
+    """Object for game of life."""
+
+    def __init__(self, size):
+        """Initialise gameboard of dimensions (size x size)."""
+        self.board = np.zeros((size, size))
+
+    def play(self):
+        """Start Game of life using initialised gameboard."""
+        print("Playing life. Press ctrl + c to stop.")
+        pyplot.ion()
+        while True:
+            self.move()
+            self.show()
+            pyplot.pause(0.0000005)
+
+    def move(self):
+        """Make a move on the gameboard."""
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
+
+        for i in range(self.board.shape[0]):
+            for j in range(self.board.shape[1]):
+                if (neighbourcount[i, j] == 3
+                    or (neighbourcount[i, j] == 2 and self.board[i, j])): # noqa F401
+                    self.board[i, j] = 1
+                else:
+                    self.board[i, j] = 0
+
+    def __setitem__(self, key, value):
+        """Set value of item on board."""
+        self.board[key] = value
+
+    def show(self):
+        """Display gameboard."""
+        pyplot.clf()
+        pyplot.matshow(self.board, fignum=0, cmap='binary')
+        pyplot.show()
+
+    def insert(self, pattern, centre):
+        """Insert pattern into board with designated centre."""
+        n = pattern.grid.shape[0]
+        self.board[centre[0]-n//2:1+centre[0]+n//2, centre[1]-n//2:1+centre[1]+n//2] = pattern.grid # noqa F401
+
+
+class Pattern:
+    """Class to hold pattern glider objects."""
+
+    def __init__(self, arr):
+        """Initialise pattern."""
+        self.grid = arr
+
+    def flip_vertical(self):
+        """Make pattern upside-down."""
+        return Pattern(np.flipud(self.grid))
+
+    def flip_horizontal(self):
+        """Make pattern reverse left-right."""
+        return Pattern(np.fliplr(self.grid))
+
+    def flip_diag(self):
+        """Transpose pattern."""
+        return Pattern(self.grid.transpose())
+
+    def rotate(self, n):
+        """Rotate pattern 90 degrees n times."""
+        return Pattern(np.rot90(self.grid, n))
